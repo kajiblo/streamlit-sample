c9ef64a5766ae6b05e133ed5a10806fde0fd2859
huarate <y.kajimoto@os.infocom.co.jp>
2023-03-10 16:08:41 +0900

    修正

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def out_fullname(hensu1,hensu2):
 if st.button("デバッグ体験",type='primary',use_container_width=True):
     fullname = out_fullname(hensu1,hensu2)
     if len(fullname) > 1:
-        st.info("あなたの名前は:" + fullname)
+        st.info(fullname)
     elif len(fullname) > 10:
         st.error('10文字以内にしてください')
     elif len(fullname) == 1:
